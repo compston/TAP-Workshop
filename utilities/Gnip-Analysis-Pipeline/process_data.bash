0fe6689c659a54a788cdc704b2f62fc2a09a1138
compston <stephen@gnipcentral.com>
2016-05-26 18:03:22 +0000

    SC: updates

--- utilities/Gnip-Analysis-Pipeline/process_data.bash
+++ utilities/Gnip-Analysis-Pipeline/process_data.bash
@@ -14,10 +14,10 @@ input_uid=aabbcc
 
 # loop over these values
 day_list=`seq -f%02g 1 31`
-month_list="01 02 03 04 05 06 07 08 09 10 11 12"
-year_list="2013 2014"
+month_list="12"
+year_list="2015"
 
-MAXPROCS=24
+MAXPROCS=8
 SLEEPTIME=2
 procName="enricher"
 
@@ -35,19 +35,22 @@ waitForNProcs()
 for year in $year_list; do
     for month in $month_list; do
         for day in $day_list; do
-            mkdir -p data/${year}/${month}/${day}
+#            mkdir -p data/${year}/${month}/${day}
             
             # Set up your processing here:
             # You might just run the enricher,
             # or just run the aggregator on enriched data.
             # Below is the full processing pipeline for daily data
             
-            input_file=data/tweets_${input_uid}.json.gz
-            if [[ "$input_file" != "" ]]; then
-                output_file=data/data_${output_uid}.pkl
-                zcat $input_file | nice ./enricher.py | ./aggregator.py > $output_file &
-                sleep 1 # sometimes helpful to slow down the initial spinup of processes
-            fi
+            zcat /mnt/Compston/20150101-20160101_pm1pv2v8ck_2015_12_${day}*activities.json.gz | nice ./enricher.py > /mnt/Compston/enriched_tweets/2015_12_${day}_enriched.json
+	    sleep 1
+
+            #input_file=data/tweets_${input_uid}.json.gz
+            #if [[ "$input_file" != "" ]]; then
+            #    output_file=data/data_${output_uid}.pkl
+            #    zcat $input_file | nice ./enricher.py | ./aggregator.py > $output_file &
+            #    sleep 1 # sometimes helpful to slow down the initial spinup of processes
+            #fi
             waitForNProcs
         done
     done
--- utilities/Gnip-Tweet-Evaluation/setup.py
+++ utilities/Gnip-Tweet-Evaluation/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup,find_packages
 
 setup(
     name='gnip_tweet_evaluation',
-    version='0.1.0',
+    version='0.1',
     author='Jeff Kolb',
     author_email='jeffakolb@gmail.com',
     packages=find_packages(),
     scripts=['evaluate_tweets.py'],
-    install_requires = ['sngrams','matplotlib'],
+    install_requires = ['sngrams','matplotlib','pyyaml','requests','requests_oauthlib','pyfarmhash'],
     url='https://github.com/jeffakolb/Gnip-Tweet-Evaluation',
     license='LICENSE.txt',
     description='Conversation and audience analysis of a corpus of Tweet payloads from Gnip/Twitter APIs.', 
